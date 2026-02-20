550958fec5fd462be1791b74defc46fb7ade2641
ChirayuP19 <chirayu010675@gmail.com>
2026-02-20 10:06:06 -0500

    Login or sigin endpoint create

--- securityDemo/src/main/java/com/example/securityDemo/controller/GreetingController.java
+++ securityDemo/src/main/java/com/example/securityDemo/controller/GreetingController.java
@@ -1,11 +1,37 @@
 package com.example.securityDemo.controller;
 
+import com.example.securityDemo.jwt.JwtUtils;
+import com.example.securityDemo.jwt.LoginRequest;
+import com.example.securityDemo.jwt.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class GreetingController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @GetMapping("/health_check")
     public String healthCheck(){
@@ -22,5 +48,35 @@ public class GreetingController {
     @GetMapping("/admin")
     public String AdminEndPoints(){
         return "hello. Admin! ";
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticationUser(@RequestBody LoginRequest loginRequest){
+        Authentication authentication;
+        try {
+            authentication =authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(
+                      loginRequest.getUsername(),
+                      loginRequest.getPassword())
+            );
+        }catch (AuthenticationException e){
+            Map<String,Object> map=new HashMap<>();
+            map.put("message","Bad credentials");
+            map.put("status",false);
+            map.put("localtimestamp", LocalDateTime.now());
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetails userDetails= (UserDetails) authentication.getPrincipal();
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+        List<String> roles = userDetails.
+                getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+        LoginResponse loginResponse = new LoginResponse(userDetails.getUsername(),jwtToken,roles);
+
+        return ResponseEntity.ok(loginResponse);
     }
 }
---
+++ securityDemo/src/main/java/com/example/securityDemo/jwt/LoginResponse.java
@@ -0,0 +1,42 @@
+package com.example.securityDemo.jwt;
+
+import java.util.List;
+
+public class LoginResponse {
+
+    public LoginResponse(String jwtToken, String username, List<String> roles) {
+        this.jwtToken = jwtToken;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    private String jwtToken;
+    private String username;
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    private List<String> roles;
+
+}
